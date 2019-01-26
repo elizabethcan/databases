9d7c73d903a224c154967e008a9abd24faf38211
elizabethcan <elizabeth230@gmail.com>
2019-01-26 15:53:23 -0800

    Complete refactoring to correctly separate concerns

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,92 +5,37 @@ var mysql = require('mysql');
 
 module.exports = {
   messages: {
+    // Ditto as above
     get: function (req, res) {
-      connection.query(`
-        SELECT * FROM messages 
-        INNER JOIN rooms 
-        ON rooms.id = messages.room_id
-      `, function (error, results, fields) {
-          if (error) { throw error; }
-          console.log('Get request messages', results);
-          res.status(200).json(results);
-        });
-    }, // a function which handles a get request for all messages
+      models.messages.get({} ,(err, data) => {
+        if (err) throw err;
+        res.status(200);
+        res.json(data);
+      });
+    },
     post: function (req, res) {
-      var text = req.body.message;
-      console.log('req.text', text);
-      debugger;
-      // If roomname was specified on object then proceed else set to lobby
-      if (req.body.roomname) {
-        // Find room by roomname
-        connection.query(`
-          SELECT * FROM rooms 
-          WHERE name="${req.body.roomname}"
-        `, function(error, results, fields) {
-          debugger;
-          if (error) {
-            throw error;
-          }
-          // if room doesn't exist create it with roomname
-          if (results.length === 0) {
-            connection.query(`
-              INSERT INTO rooms(name)
-              VALUES ("${req.body.roomname}")
-            `, (error, results, fields) => {
-              // Once room is created proceed with creating the message
-              var roomId = results.insertId;
-              connection.query(`
-              INSERT INTO messages(text, room_id)
-              VALUES ("${text}", "${roomId}");
-            `, function (error, results, fields) {
-                if (error) { throw error; }
-                connection.query(`SELECT * FROM messages WHERE id=${results.insertId}`, function (error, results, fields) {
-                  if (error) { throw error; }
-                  res.status(201).json(results);
-                });
-              });
-            });
-          }
-        });
-      } else {
-        // Set roomname to lobby
-        connection.query(`
-          INSERT INTO messages(text)
-          VALUES ("${text}");
-        `, function (error, results, fields) {
-          if (error) { throw error; }
-          connection.query(`SELECT * FROM messages WHERE id=${results.insertId}`, function (error, results, fields) {
-            if (error) { throw error; }
-            res.status(201).json(results);
-          });
-        });
-      }
-    } // a function which handles posting a message to the database
+      models.messages.post(req.body, (err, data) => {
+        if (err) throw err;
+        res.status(201);
+        res.json(data);
+      });
+    }
   },
 
   users: {
     // Ditto as above
     get: function (req, res) {
-      connection.query('SELECT * FROM users', function (error, results, fields) {
-        if (error) { throw error; }
-        console.log(results[0]);
-        res.status(200).json(results);
+      models.users.get({} ,(err, data) => {
+        if (err) throw err;
+        res.status(200);
+        res.json(data);
       });
     },
     post: function (req, res) {
-      console.log(req.body);
-      var username = req.body.username;
-      console.log(username);
-      var query = `
-        INSERT INTO users(name)
-        VALUES ('${username}');
-      `;
-      connection.query(query, function (error, results, fields) {
-        if (error) { throw error; }
-        connection.query(`SELECT * FROM users WHERE id=${results.insertId}`, function (error, results, fields) {
-          if (error) { throw error; }
-          res.status(201).json(results);
-        });
+      models.users.post(req.body, (err, data) => {
+        if (err) throw err;
+        res.status(201);
+        res.json(data);
       });
     }
   },
@@ -107,7 +52,7 @@ module.exports = {
     post: function (req, res) {
       models.rooms.post(req.body, (err, data) => {
         if (err) throw err;
-        res.status(200);
+        res.status(201);
         res.json(data);
       });
     }
--- server/models/index.js
+++ server/models/index.js
@@ -1,37 +1,113 @@
 var db = require('../db');
 var models = {
   messages: {
-    get: function (callback) {
+    get: function (options, callback) {
       db.query(`
         SELECT * FROM messages
-      `, () => {
-
+      `, (error, results) => {
+        if (error) {
+          callback(error);
+        } else {
+          callback(null, results);
+        }
       });
     }, // a function which produces all the messages
     post: function (data, callback) {
-      models.rooms.get(callback, {name: data.roomname, id: data.id})
-      db.query(`
-
-      `, () => {
-        
-      });
+      models.rooms.post({'name': data.roomname}, (err, results) => {
+        var roomId = results;
+        models.users.post({'name': data.username}, (err, results) => {
+          var userId = results;
+          var query = `
+            INSERT messages(text, room_id, user_id) VALUES (?, ?, ?)
+          `;
+          var queryArgs = [data.message, roomId, userId]
+          db.query(query, queryArgs, (error, results) => {
+            if (error) {
+              callback(error);
+            } else {
+              callback(null, results);
+            }
+          });
+        })
+      })
     } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function (callback) {
-      db.query(`
-        SELECT * from users
-      `, () => {
-        
-      });
+    get: function (options, callback) {
+      // options {
+      //   id: '1'
+      //   name: 'lobby'
+      // }
+      console.log(options);
+      if (options.id) {
+        // get one room by id
+        var queryString = `
+          SELECT * FROM users
+          WHERE users.id = ?
+        `;
+        var queryArgs = [options.id];
+        // Query
+        db.query(queryString, queryArgs, (error, results) => {
+          if (error) {
+            callback(error);
+          } else {
+            callback(null, results);
+          }
+        });
+      } else if (options.name) {
+        console.log('searching by name:', options.name)
+        // get all users
+        var queryString = `
+          SELECT * FROM users
+          WHERE users.name = ?
+        `;
+        var queryArgs = [options.name];
+        db.query(queryString, queryArgs, (error, results) => {
+          if (error) {
+            callback(error);
+          } else {
+            callback(null, results);
+          }
+        });
+      } else {
+        // get all users
+        db.query(`
+          SELECT * FROM users
+        `, (error, results) => {
+          if (error) {
+            callback(error);
+          } else {
+            callback(null, results);
+          }
+        });
+      }
     },
-    post: function (callback) {
-      db.query(`
-
-      `, () => {
-        
+    post: function (data ,callback) {
+      if (data.username) {
+        var name = data.username
+      } else {
+        var name = data.name;
+      }
+      models.users.get({
+        'name': name
+      }, (err, results) => {
+        if (results.length === 0) {
+          var query = `
+          INSERT users (name) VALUES (?)
+          `;
+          var queryArgs  = [name]
+          db.query(query, queryArgs, (error, results) => {
+            if (error) {
+              callback(error);
+            } else {
+              callback(null, results.insertId);
+            }
+          });
+        } else {
+          callback(null, results[0].id);
+        }
       });
     }
   },
@@ -106,11 +182,11 @@ var models = {
             if (error) {
               callback(error);
             } else {
-              callback(null, results);
+              callback(null, results.insertId);
             }
           });
         } else {
-          callback(null);
+          callback(null, results[0].id);
         }
       });
     }
