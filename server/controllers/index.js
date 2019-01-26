7670dceb6fb5b466be7a86311ef65372f038792a
elizabethcan <elizabeth230@gmail.com>
2019-01-25 20:08:22 -0800

    Add rooms routes

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -13,31 +13,119 @@ connection.connect();
 module.exports = {
   messages: {
     get: function (req, res) {
-      connection.query('SELECT * FROM messages', function (error, results, fields) {
-        if (error) { throw error; }
-        console.log(results[0]);
-        res.status(200).json(results);
-      });
-    }, // a function which handles a get request for all messages
-    post: function (req, res) {
-      var query = `
-        INSERT INTO messages(text)
-        VALUES ('Hello Again');
-      `;
-      connection.query(query, function (error, results, fields) {
-        if (error) { throw error; }
-        connection.query(`SELECT * FROM messages WHERE id=${results.insertId}`, function (error, results, fields) {
+      connection.query(`
+        SELECT * FROM messages 
+        INNER JOIN rooms 
+        ON rooms.id = messages.room_id
+      `, function (error, results, fields) {
           if (error) { throw error; }
+          console.log(results);
           res.status(200).json(results);
         });
-      });
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      var text = req.body.message;
+      console.log(text);
+      // Query to check user and if that user didn't exist create on
+      // get user id
+      // '' rooms
+      // Check if roomname exists
+      if (req.body.roomname) {
+        // Find room by roomname
+        connection.query(`
+          SELECT * FROM rooms 
+          WHERE name="${req.body.roomname}"
+        `, function(error, results, fields) {
+          if (error) {
+            throw error;
+          }
+          // if room doesn't exist create it with roomname
+          if (results.length === 0) {
+            connection.query(`
+              INSERT INTO rooms(name)
+              VALUES ("${req.body.roomname}")
+            `, (error, results, fields) => {
+              // Once room is created proceed with creating the message
+              var roomId = results.insertId;
+              connection.query(`
+              INSERT INTO messages(text, room_id)
+              VALUES ("${text}", "${roomId}");
+            `, function (error, results, fields) {
+                if (error) { throw error; }
+                connection.query(`SELECT * FROM messages WHERE id=${results.insertId}`, function (error, results, fields) {
+                  if (error) { throw error; }
+                  res.status(201).json(results);
+                });
+              });
+            });
+          }
+        });
+      } else {
+        // Set roomname to lobby
+        connection.query(`
+          INSERT INTO messages(text)
+          VALUES ("${text}");
+        `, function (error, results, fields) {
+          if (error) { throw error; }
+          connection.query(`SELECT * FROM messages WHERE id=${results.insertId}`, function (error, results, fields) {
+            if (error) { throw error; }
+            res.status(201).json(results);
+          });
+        });
+      }
     } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      connection.query('SELECT * FROM users', function (error, results, fields) {
+        if (error) { throw error; }
+        console.log(results[0]);
+        res.status(200).json(results);
+      });
+    },
+    post: function (req, res) {
+      console.log(req.body);
+      var username = req.body.username;
+      console.log(username);
+      var query = `
+        INSERT INTO users(name)
+        VALUES ('${username}');
+      `;
+      connection.query(query, function (error, results, fields) {
+        if (error) { throw error; }
+        connection.query(`SELECT * FROM users WHERE id=${results.insertId}`, function (error, results, fields) {
+          if (error) { throw error; }
+          res.status(201).json(results);
+        });
+      });
+    }
+  },
+
+  rooms: {
+    // Ditto as above
+    get: function (req, res) {
+      connection.query('SELECT * FROM rooms', function (error, results, fields) {
+        if (error) { throw error; }
+        console.log(results[0]);
+        res.status(200).json(results);
+      });
+    },
+    post: function (req, res) {
+      var roomname = req.body.roomname;
+      var query = `
+        INSERT INTO rooms(name)
+        VALUES (${roomname});
+      `;
+      connection.query(query, function (error, results, fields) {
+        if (error) { throw error; }
+        connection.query(`SELECT * FROM roomname WHERE id=${results.insertId}`, function (error, results, fields) {
+          if (error) { throw error; }
+          res.status(201).json(results);
+        });
+      });
+    }
   }
 };
 
--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -66,14 +66,15 @@ describe('Persistent Node Chat Server', function() {
   });
 
   it('Should output all messages from the DB', function(done) {
-    // Let's insert a message into the db
-    var queryString = '';
-    var queryArgs = [];
-    // TODO - The exact query string and query args to use
-    // here depend on the schema you design, so I'll leave
-    // them up to you. */
-
-    dbConnection.query(queryString, queryArgs, function(err) {
+    request.post({
+      method: 'POST',
+      uri: 'http://127.0.0.1:3000/classes/messages',
+      json: {
+        username: 'Valjean',
+        message: 'Men like you can never change!',
+        roomname: 'main'
+      }
+    }, function(err) {
       if (err) { throw err; }
 
       // Now query the Node chat server and see if it returns
@@ -81,7 +82,7 @@ describe('Persistent Node Chat Server', function() {
       request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
         var messageLog = JSON.parse(body);
         expect(messageLog[0].text).to.equal('Men like you can never change!');
-        expect(messageLog[0].roomname).to.equal('main');
+        expect(messageLog[0].name).to.equal('main');
         done();
       });
     });
