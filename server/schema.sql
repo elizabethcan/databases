7837d2288d225ba1ed826a741bb718df406b4fc1
elizabethcan <elizabeth230@gmail.com>
2019-01-25 17:21:41 -0800

    Setup initial database tables

--- server/schema.sql
+++ server/schema.sql
@@ -2,14 +2,27 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(20),
+  PRIMARY KEY (id)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(20),
+  PRIMARY KEY (id)
+);
 
-
-
+CREATE TABLE messages (
+  id INT AUTO_INCREMENT,
+  text VARCHAR(255),
+  user_id INT,
+  room_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
